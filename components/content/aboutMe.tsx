cb54bf210dc24e01322f41a728fcacc32c06e331
Dan Thompson <thompsondanielk@gmail.com>
2023-09-21 14:26:57 -0400

    removed menu border, added about me content

--- components/content/aboutMe.tsx
+++ components/content/aboutMe.tsx
@@ -1,3 +1,24 @@
 export const AboutMe = () => {
-  return <>About me</>;
+  return (
+    <div className="text-content">
+      <p>
+        My name is Daniel Thompson. I am a Software Engineer with a track record
+        of developing innovative technical solutions. My career journey has been
+        marked by a collaborative spirit, thriving in diverse teams alongside
+        engineers, product experts, and designers to fuse ideas into seamless
+        solutions. Based in Columbus, Ohio, I recently played a pivotal role at
+        eFuse, where I leveraged my expertise in MongoDB, Express, Redis,
+        Node.js, React, and GraphQL to bolster an esports tournament platform
+        while spearheading SEO and analytics enhancements, contributing
+        significantly to EsportsGG&apos;s remarkable growth.
+      </p>
+      <p>
+        My versatility extends to Salesforce, C#, and other technologies, which
+        I expertly utilized during my tenure at RevLocal. Equipped with a strong
+        passion for crafting exceptional user experiences, I am eager to embark
+        on new opportunities and collaborate with exceptional individuals and
+        teams.
+      </p>
+    </div>
+  );
 };
